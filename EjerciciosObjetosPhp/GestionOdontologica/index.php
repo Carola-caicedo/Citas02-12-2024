9b5c4a2e51bde5ccd6d1cc47c4c88bcd984fb9be
Carola-caicedo <carolacaicedo1527@gmail.com>
2024-12-02 10:12:11 -0500

    Add files via upload

---
+++ EjerciciosObjetosPhp/GestionOdontologica/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <body>
+        <?php
+        require_once 'Controlador/Controlador.php';
+        require_once 'Modelo/GestorCita.php';
+        require_once 'Modelo/Cita.php';
+        require_once 'Modelo/Paciente.php';
+        require_once 'Modelo/Conexion.php';
+
+        $controlador = new Controlador();
+
+        if (isset($_GET["accion"])) {
+            if ($_GET["accion"] == "asignar") {
+                $controlador->verPagina('Vista/html/asignar.php');
+            }
+            if ($_GET["accion"] == "consultar") {
+                $controlador->verPagina('Vista/html/consultar.php');
+            }
+            if ($_GET["accion"] == "cancelar") {
+                $controlador->verPagina('Vista/html/cancelar.php');
+            }
+            
+            elseif ($_GET["accion"] == "guardarCita") {
+                $controlador->agregarCita(
+                    $_POST["asignarDocumento"],
+                    $_POST["medico"],
+                    $_POST["fecha"],
+                    $_POST["hora"],
+                    $_POST["consultorio"]
+                );
+            }
+        } else {
+            $controlador->verPagina('Vista/html/inicio.php');
+        }
+        ?>
+    </body>
+</body>
+
+</html>
